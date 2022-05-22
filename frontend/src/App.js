a12a175135f3d245cf92de72b2a16a090bf5f2ce
poyanpayandeh <poyanpayandeh@hotmail.com>
2022-05-23 05:25:57 +1200

    re installed react app
    react app was broken and wasn't working so I had to remove and re create it.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,25 @@
-import React from "react";
-import {
-  Route,
-  NavLink,
-  BrowserRouter as Router,
-  Redirect
-} from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-import Home from "./pages/Home";
-import SEPractice from "./pages/SE-Practice";
-import SubmitArticle from "./pages/Submit-Article"; 
-import NotFoundPage from "./pages/404";
-
-const App = () =>  {
-    return (
-        <Router>
-        <div>
-          <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
-            <ul className="header">
-                <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
-                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
-            </ul>
-          <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
-            <Route  path="/SubmitArticle" component={SubmitArticle}/>
-            <Route exact path="/404" component={NotFoundPage}/>
-            <Redirect to="/404" />
-
-          </div>
-        </div>
-        </Router>
-    );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
- 
+
 export default App;
