1733f7963bee2ad4c05a2c9b699a0c381ade9f16
shuoy123 <89519840+shuoy123@users.noreply.github.com>
2022-06-03 06:17:20 +1200

    update

---
+++ frontend/src/components/FindForm.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const FindForm = () => {
+  const { register, handleSubmit } = useForm();
+  const [result, setResult] = useState("");
+  const onSubmit = (data) => setResult(JSON.stringify(data));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <p>Input the keyword to find the artical</p>
+    <p>Title:<input {...register("title")} placeholder="Title" /></p>
+    <p>Author<input {...register("authors")} placeholder="Authors" /></p>
+    <p>Journal<input {...register("journal")} placeholder="Journal" /></p>
+     
+     
+ 
+      <input type="submit" value="Find"/>
+    </form>
+  );
+}
+export default FindForm;
