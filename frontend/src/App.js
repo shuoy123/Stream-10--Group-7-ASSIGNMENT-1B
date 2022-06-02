400a8d37c0f676385643a2e223a192e85fdf58af
shuoy123 <89519840+shuoy123@users.noreply.github.com>
2022-06-02 14:27:21 +1200

    Addshuo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,101 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Axios from "axios";
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import {Route, NavLink, BrowserRouter as Router, Redirect} from "react-router-dom";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
 
-// function App() {
-//   // const navigate = useNavigate();
-//   function createPost() {
-//     //Takes the currents users id and name
-
-//     let title = "Bob";
-//     let author = "Dug";
-//     let number = 4;
-//     let pages = 2;
-
-//     const config = {
-//       header: {
-//         "Content-Type": "application/json",
-//       },
-//     };
-
-//     try {
-//       const { data } = axios.post(
-//         "http://localhost:5000/",
-
-//         { title, author, number, pages },
-
-//         config
-//       );
-
-//       alert("Post!");
-
-//       localStorage.setItem("authToken", data.token);
-//     } catch (error) {
-//       // console.log(error.response.data);
-
-//       setTimeout(() => {}, 1000);
-//     }
-//   }
-
-//   return (
-//     <div className="app_container">
-//       <h1 className="title">assignment 2</h1>
-//       <p>here is some information</p>
-//       <button onClick={createPost}> Press me</button>
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-//   );
-// }
-
-// export default App;
-
-function App() {
-  Axios({
-    method: "GET",
-    url: "http://localhost:5000/",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    console.log(res.data.message);
-  });
-
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+      <div>
+        <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
+          <ul className="header">
+              <li><NavLink exact to = "/">Home</NavLink></li>
+              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+              <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+          </ul>
+        <div className="content">
+          <Route exact path="/" component={Home}/>
+          <Route  path="/SEPractice" component={SEPractice}/>
+          <Route  path="/SubmitArticle" component={SubmitArticle}/>
+          <Route exact path="/404" component={NotFoundPage}/>
+          <Redirect to="/404" />
+        </div>
+      </div>
+      </Router>
   );
 }
 
 export default App;
+
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Stream-10--Group-7-ASSIGNMENT-1B</h2>
+        <p>Shuo Yang</p>
+        <p>Liam Burr-Knight</p>
+        <p>Poyan Payandeh</p>
+      </div>
+    );
+  }
+}
+export default Home;
