e08aae4df62c372fe15b83108f0b94971d4dd39e
shuoy123 <89519840+shuoy123@users.noreply.github.com>
2022-06-03 06:20:50 +1200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {Route, NavLink, BrowserRouter as Router, Redirect} from "react-router-dom";
 import Home from "./pages/Home";
-import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
+import ModerationArticle from "./pages/Moderation-Attical"; 
+
+import FindArticle from "./pages/Find-Artical"; 
 import NotFoundPage from "./pages/404";
 
 const App = () =>  {
@@ -12,13 +14,22 @@ const App = () =>  {
         <h1>Software Engineering Practice Evidence Repository (SEPER)</h1>
           <ul className="header">
               <li><NavLink exact to = "/">Home</NavLink></li>
-              <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+             
               <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
+              <li><NavLink to = "/FindArticle">Find an Article</NavLink></li>
+              <li><NavLink to = "/ModerationArticle">Moderation an Article</NavLink></li>
+            
           </ul>
+          
+         
         <div className="content">
           <Route exact path="/" component={Home}/>
-          <Route  path="/SEPractice" component={SEPractice}/>
+        
           <Route  path="/SubmitArticle" component={SubmitArticle}/>
+          <Route  path="/FindArticle" component={FindArticle}/>
+          <Route  path="/ModerationArticle" component={ModerationArticle}/>
+     
+          
           <Route exact path="/404" component={NotFoundPage}/>
           <Redirect to="/404" />
         </div>
